b1d35cb857019fd75177070796904ed7ec606c53
RLis <lisss17@mail.ru>
2024-04-01 19:01:23 +0500

    add changeTheme (useState-UseEffect)

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { MonthStatistics } from "./pages/MonthStatistics/components/MonthStatist
 import { Header } from "./pages/shared/Header/Header";
 import './styles/index.scss';
 import { Popup } from "./pages/shared/Popup/Popup";
+import { ThemeProvider } from "./provider/ThemeProvider";
 
 
 function App() {
   return (
     <div className='container'>
-      <Popup />
-      <Header />
-      <Routes>
-          <Route path='/' Component={Home}/>
-          <Route path='/month-statistics' Component={MonthStatistics}/>
-        </Routes>
+      <ThemeProvider>
+        {/* <Popup /> */}
+        <Header />
+        <Routes>
+            <Route path='/' Component={Home}/>
+            <Route path='/month-statistics' Component={MonthStatistics}/>
+          </Routes>
+      </ThemeProvider>
     </div>
   );
 }
--- src/pages/shared/Header/Header.tsx
+++ src/pages/shared/Header/Header.tsx
@@ -1,30 +1,65 @@
-import s from "./Header.module.scss";
-import { GlobalSvgSelector } from "../../../assets/images/icons/global/GlobalSvgSelector";
+import { useEffect, useState } from "react"
+import s from "./Header.module.scss"
+import { GlobalSvgSelector } from "../../../assets/images/icons/global/GlobalSvgSelector"
 import Select from 'react-select'
+import { log } from "console";
+import { compileFunction } from "vm";
 
 interface Props {}
 
 
-const options = [
-  { value: 'city-1', label: 'Санкт-Петербург' },
-  { value: 'city-2', label: 'Москва' },
-  { value: 'city-3', label: 'Екатеринбург' }
-]
 
-const colorStyles = {
-  control: (styles: any) => ({...styles, 
-    backgroundColor: 'rgba(71, 148, 255, 0.20)', 
-    borderRadius: '10px',
-    fontSize: '16px',
-    width: '194px',
-    textAlign: 'center',
-    color: '#000',
-    border: 'none',
-    zIndex: '100'
-  })
-}
 
 export const Header = (props: Props) => {
+
+  const [theme, setTheme] = useState('light');
+
+  const options = [
+    { value: 'city-1', label: 'Санкт-Петербург' },
+    { value: 'city-2', label: 'Москва' },
+    { value: 'city-3', label: 'Екатеринбург' }
+  ]
+  const colorStyles = {
+    control: (styles: any) => ({
+      ...styles, 
+      backgroundColor: theme === 'dark' ? '#4f4f4f' : 'rgba(71, 148, 255, 0.20)', 
+      borderRadius: '10px',
+      fontSize: '16px',
+      width: '194px',
+      textAlign: 'center',
+      color: '#000',
+      border: 'none',
+      zIndex: '100'
+    }),
+    singleValue: (styles: any) => ({
+      ...styles, 
+      color:  theme === 'dark'  ? '#fff': '#000',
+      zIndex: '100'
+    })
+  }
+
+
+
+  const changeTheme = () => { 
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+ //используем useEffect, чтобы не зависеть от асинхронного useState
+  useEffect(() => {
+    
+    const root = document.querySelector(':root') as HTMLElement
+
+    const componentsStyles = ['--body-background-', '--components-background-', '--card-background-', '--text-color-', '--box-shadow-']
+
+    componentsStyles.forEach( (component => {
+      root.style.setProperty(
+      `${component}default`,  // первый парметр - что меняем
+        `var(${component}${theme})` // второй - НА что меняем значение
+        )
+    }))
+
+  }, [theme])
+ 
+
   return (
     <section className={s.header}>
       <div className={s.wrapper}>
@@ -34,7 +69,10 @@ export const Header = (props: Props) => {
         <div className={s.title}>City Weather</div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change_theme}>
+        <div  
+          className={s.change_theme}
+          onClick={changeTheme}
+        >
           <GlobalSvgSelector id="change_theme" />
         </div>
         <Select 
